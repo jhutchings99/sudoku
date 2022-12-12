9da772f21d9ca4850c889976c33bc945f0de22d7
jhutchings99 <d00352989@dmail.dixie.edu>
2022-12-12 16:20:21 -0700

    board mostly working

--- client/app.js
+++ client/app.js
@@ -1,5 +1,27 @@
-var app = new Vue({
-        el: "#app",
-        data: {},
-    methods: {},
-})
+const app = new Vue({
+    el: '#app',
+    data: {
+        board: [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+    },
+    methods: {
+        checkInput: function (event, rowIndex, columnIndex) {
+            if (event.target.value > 9 || event.target.value < 1) {
+                this.board[rowIndex][columnIndex] = 0;
+            }
+        },
+
+        editCell: function (event, rowIndex, columnIndex) {
+            this.board[rowIndex][columnIndex] = event.target.value;
+        }
+    }
+});
